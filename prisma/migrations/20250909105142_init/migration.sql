23ff7864521ba142d6e03b571500b818a257a457
syifacts <ss4166269@gmail.com>
2025-09-09 18:01:51 +0700

    schema baru

--- prisma/migrations/20250909105142_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `Answer` ADD CONSTRAINT `Answer_preferenceQuestionCode_fkey` FOREIGN KEY (`preferenceQuestionCode`) REFERENCES `PreferenceQuestion`(`code`) ON DELETE SET NULL ON UPDATE CASCADE;
