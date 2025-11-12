969fe0e6a191bac9450d9e4eee870497f513c5d6
syifacts <ss4166269@gmail.com>
2025-11-12 21:30:14 +0700

    new eslint

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -1,12 +1,28 @@
+// import { prisma } from "@/lib/prisma";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+//   try {
+//     const id = parseInt(params.id);
+
+//     const user = await prisma.user.delete({
+//       where: { id },
+//     });
+
+//     return NextResponse.json({ success: true, user });
+//   } catch (err: any) {
+//     console.error(err);
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
-    const id = parseInt(params.id);
-
+    const { id } = await context.params; // ⬅️ perhatikan: pakai await
     const user = await prisma.user.delete({
-      where: { id },
+      where: { id: parseInt(id) },
     });
 
     return NextResponse.json({ success: true, user });
